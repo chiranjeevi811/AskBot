5ba60091f5be87240373cad6d0da9a3aef3ac9dc
Chiranjeevi Adithya <chiranjeevi210606@gmail.com>
2026-07-07 19:40:20 +0530

    Ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 
 import './App.css'
 // import Chat from './Chat'
+import { useState } from 'react';
 import SideBar from './SideBar'
 import { MyContext } from './MyContext'
 import ChatWindow from './ChatWindow';
+import { v1 as uuidv1 } from "uuid";
 function App() {
-  const providerValues = {};
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+
+
+  const providerValues = {
+    prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setCurrThreadId
+  };
 
   return (
     <div className='app'>
